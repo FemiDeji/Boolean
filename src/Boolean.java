99c08bc43f4bd91e12b3158f60af9521dc7dd1c8
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 14:46:37 -0800

    Initial commit

---
+++ src/Boolean.java
@@ -0,0 +1,9 @@
+public class Boolean{
+    public static void main(String[] args){
+        boolean var1 = true;
+        if(var1 = true)//check if the value is true or false
+            System.out.println("Boolean value is True");
+        else
+            System.out.println("Boolean value is false");
+    }
+}
